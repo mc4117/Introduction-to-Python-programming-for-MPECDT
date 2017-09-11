94a3b59fbbcced0e669d43aa5c775acc5a4c4043
Mariana Clare <mdac12@hotmail.co.uk>
2017-09-11 13:06:59 +0100

    python lesson 4 including script to split columns

--- src/mean.py
+++ src/mean.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 # Open data file
 infile = open("../data/data1.txt", "r")  # ".." means "go up one directory"
 
@@ -7,9 +9,9 @@ n = 0
 
 # Loop to perform sum
 for number in infile:
-    number = float(number)
+    number = float(number) # this is because the numbers are strings
     mean = mean + number
-    n += 1
+    n += 1 # n = n + 1 (this is an inplace assignment operator)
 
 # It is good practice to close a file when you are finished.
 infile.close()
